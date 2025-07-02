6c22ae3e42be2951d65e3013f2ff6aabaed91037
Usenmfon <usenmfonuko@gmail.com>
2025-07-02 19:12:21 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Task Tracker CLI
+ * Usage examples:
+ *   php task-cli.php add "Buy groceries"
+ *   php task-cli.php update 1 "Buy groceries and cook dinner"
+ *   php task-cli.php delete 1
+ *   php task-cli.php mark-in-progress 1
+ *   php task-cli.php mark-done 1
+ *   php task-cli.php list
+ *   php task-cli.php list done
+ */
